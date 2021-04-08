472a8f0b652a90a9e0e51417800970e5ad5c282d
yfd <670056495@qq.com>
2021-04-08 16:05:18 +0800

    完成了玩家下棋后电脑随机选择一个位置下棋的功能

--- src/components/Chessboard.vue
+++ src/components/Chessboard.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="chessboard_container">
     <template>
-      <ul v-for="(item, index) of this.$store.state.chessman_list" :key="index">
-        <li v-for="obj of item" :key="obj.index"  @click="paly_chess($event, obj.index)">
-          <span :data-num="obj.index"></span>
+      <ul v-for="(item, index) of this.chessList" :key="index">
+        <li v-for="obj of item" :key="obj.index" :class="className(obj.value)" @click="paly_chess($event, obj.index)">
+          <span></span>
         </li>
       </ul>
     </template>
@@ -11,21 +11,59 @@
 </template>
 
 <script>
+import { mapMutations, mapGetters } from 'vuex';
+
 export default {
   data () {
     return {
       cal: 0,
       // 动态组件
       dymamicComponent: '',
-      color: 'false'
+      color: 'false',
+      // 人机下棋切换
+      curPlayer: 'player'
+    }
+  },
+  computed: {
+    ...mapGetters(['chessList']),
+    className () {
+      console.log('exe computerFn');
+      return value => {
+        console.log(value);
+        if (!value) return ''
+        return value === 'player' ? 'chessmanBlue' : 'chessmanRed'
+      }
     }
   },
   methods: {
+    // 辅助函数映射
+    ...mapMutations(['set_mode', 'set_role', 'change_value']),
+    // 电脑下棋的逻辑
+    copmputerPlay (index) {
+      // index 参数是从pay_chess函数传递的玩家自主决定的下棋的位置的index
+      // 电脑怎么判断在什么地方下棋
+      // 首先应该是判断自己现在能不能赢
+      const leftList = this.chessList.flat().filter(ele => ele.value === '');
+      // 随机挑选这个数组中的一个对象的index作为index传入
+      if (!leftList.length) return
+      const randomNum = Math.floor(Math.random() * leftList.length);
+      const indexPara = leftList[randomNum].index;
+      this.change_value({ index: indexPara, value: 'computer' });
+      console.table(this.chessList.flat());
+    },
+
+    // 玩家执棋
     paly_chess (e, index) {
-      const curValue = e.currentTarget.childNodes[0].dataset.num;
-      if (curValue === index.toString()) {
-        e.currentTarget.classList.add('chessmanBlue');
-      };
+      // 判断当前这地方是否已经有棋子
+      if (this.chessList.flat()[index - 1].value) return
+      // 改变当前点击的格子的状态value，确定是玩家自己下棋的格子
+      this.change_value({ index, value: 'player' });
+      this.copmputerPlay(index);
+    },
+
+    // 关于判断是否有一方已经赢了的问题
+    winConditions () {
+      // 横向三个为一样的话，就赢了，游戏结束
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -9,17 +9,44 @@ const store = new Vuex.Store({
     mode: 'single(default)',
     // 选择了X还是O
     role: 'X(default)',
-    // 棋子记录数组
+    // 棋子记录数组,index是对应棋盘的位置index，value则记录棋子是哪一方的
     chessman_list: [
       [{ value: '', index: 1 }, { value: '', index: 2 }, { value: '', index: 3 }], [{ value: '', index: 4 }, { value: '', index: 5 }, { value: '', index: 6 }], [{ value: '', index: 7 }, { value: '', index: 8 }, { value: '', index: 9 }]]
+  },
+  getters: {
+    chessList (state) {
+      return state.chessman_list;
+    },
+    // 根据index拿到对应的那个对象
+    findObj (state, index) {
+      if (!state.chessman_list) return
+      return state.chessman_list.forEach(ele => {
+        return ele.find(el => el.index === index)
+      })
+    }
   },
   // 同步改变state
   mutations: {
     set_mode (state, val) {
       state.mode = val
     },
+
     set_role (state, val) {
       state.role = val
+    },
+
+    // 改变原始数据，更新组件里的状态
+    change_value (state, payload) {
+      if (!store.getters.chessList) return
+      // 首先确定改变的对象在那一行，再确定在那一列
+      state.chessman_list.forEach(ele => {
+        if (!ele) return
+        ele.forEach(el => {
+          if (el.index === payload.index) {
+            Vue.set(el, 'value', payload.value);
+          }
+        })
+      })
     }
   }
 })
